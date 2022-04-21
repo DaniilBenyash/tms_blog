a24bb6875af5a520c7b401872083f3eb5e095bd5
DaniilBenyash <daniilbenyash22@gmail.com>
2022-04-21 20:17:08 +0300

    fix: buttons

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,13 +8,12 @@ type ButtonProps = {
     disable: boolean,
     icon?: string 
 }
-const getIcon = (icon: string | undefined): React.ReactElement | undefined => {
-    if(icon) return <img src={icon} alt='' className='icons'/>
-
-    return 
-}   
+ 
 export const Button = ({onClick, text, className, disable, icon}: ButtonProps) => {
     return (
-        <button type='button' onClick={onClick} className={className} disabled={disable}>{getIcon(icon)}{text}</button>
+        <button type='button' onClick={onClick} className={className} disabled={disable}>
+            {icon && <img src={icon} alt='' className='icons'/>}
+            {text}
+        </button>
     )
 }
