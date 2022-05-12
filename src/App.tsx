43b385bff40e30a7ad33db109b79e8d26a3ad767
DaniilBenyash <daniilbenyash22@gmail.com>
2022-05-12 20:58:47 +0300

    feat: add Link to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Button } from './components/Button/Button'
-import { ReactComponent as Bookmark } from './components/Button/icon/like.svg'
-import { ReactComponent as Like } from './components/Button/icon/like.svg'
-import { ReactComponent as Dislike } from './components/Button/icon/dislike.svg'
+import { Link } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className="App">
-        <Button text='Primary' onClick={() => console.log('Text')} className='button--primary' disabled={false}/>
-        <Button text='Secondary' onClick={() => console.log('Text')} className='button--secondary' disabled={false}/>
-        <Button text='Secondary 2' onClick={() => console.log('Text')} className='button--secondary2' disabled={false}/>
-        <Button text='Button with icon' onClick={() => console.log('Text')} className='button--with_icon' disabled={false} icon={<Bookmark />}/>
-        <Button onClick={() => console.log('Text')} className='button--like' disabled={false} icon={<Like />}/>
-        <Button onClick={() => console.log('Text')} className='button--dislike' disabled={false} icon={<Dislike />}/>
+      <nav>
+        <Link to="/posts">Post</Link>
+        <Link to="/posts/1">Post 1</Link>
+        <Link to="/posts/add">Post 1</Link>
+        <Link to="/search">Search</Link>
+      </nav>
     </div>
   );
 }
