e55cbca8e420c1c639050aa6d410d68bf6d76b22
DaniilBenyash <daniilbenyash22@gmail.com>
2022-05-16 20:35:23 +0300

    feat: change theme

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import { Button } from './components/Button/Button'
 import { ReactComponent as Bookmark } from './components/Button/icon/like.svg'
 import { ReactComponent as Like } from './components/Button/icon/like.svg'
 import { ReactComponent as Dislike } from './components/Button/icon/dislike.svg'
+import { setTheme } from './redux/reducers/theme'
+import { useAppSelector, useAppDispatch } from './redux/hooks';
 
 function App() {
 
-  const [theme, setTheme] = useState('dark');
+  const theme = useAppSelector(state => state.theme.value)
+  const dispatch = useAppDispatch()
 
-  const toggleThme = () => {
+  const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.target)
+  }
+
+  const toggleTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark'
-    setTheme(newTheme)
+    dispatch(setTheme(newTheme))
   }
   return (
     <div className={`App theme--${theme}`}>
-        <Button text='Primary' onClick={toggleThme} className='button--primary' disabled={false}/>
+        <Button text='Primary' onClick={toggleTheme} className='button--primary' disabled={false}/>
         <Button text='Secondary' onClick={() => console.log('Text')} className='button--secondary' disabled={false}/>
         <Button text='Secondary 2' onClick={() => console.log('Text')} className='button--secondary2' disabled={false}/>
         <Button text='Button with icon' onClick={() => console.log('Text')} className='button--with_icon' disabled={false} icon={<Bookmark />}/>
