8d4931f41ed694b7c248fcf56dfcc940a00291bc
DaniilBenyash <daniilbenyash22@gmail.com>
2022-05-16 20:15:36 +0300

    feat: add change theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from './components/Button/Button'
 import { ReactComponent as Bookmark } from './components/Button/icon/like.svg'
 import { ReactComponent as Like } from './components/Button/icon/like.svg'
@@ -6,9 +6,15 @@ import { ReactComponent as Dislike } from './components/Button/icon/dislike.svg'
 
 function App() {
 
+  const [theme, setTheme] = useState('dark');
+
+  const toggleThme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark'
+    setTheme(newTheme)
+  }
   return (
-    <div className="App">
-        <Button text='Primary' onClick={() => console.log('Text')} className='button--primary' disabled={false}/>
+    <div className={`App theme--${theme}`}>
+        <Button text='Primary' onClick={toggleThme} className='button--primary' disabled={false}/>
         <Button text='Secondary' onClick={() => console.log('Text')} className='button--secondary' disabled={false}/>
         <Button text='Secondary 2' onClick={() => console.log('Text')} className='button--secondary2' disabled={false}/>
         <Button text='Button with icon' onClick={() => console.log('Text')} className='button--with_icon' disabled={false} icon={<Bookmark />}/>
