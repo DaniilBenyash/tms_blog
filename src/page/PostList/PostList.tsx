59d43d47f018c22a5f62f40fa978732702c70e16
DaniilBenyash <daniilbenyash22@gmail.com>
2022-05-19 17:33:33 +0300

    fix: width and height of text and Title

--- src/page/PostList/PostList.tsx
+++ src/page/PostList/PostList.tsx
@@ -25,7 +25,7 @@ const postsFormServer: Post[] = [
         "text": 'Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.',
         "date": "2021-10-06",
         "lesson_num": 123,
-        "title": 'Astronauts prep for new solar arrays on nearly seven-hour spacewalk',
+        "title": 'Astronauts prep for new solar arrays on nearly seven-hour space',
         "author": 7
     },
     {
