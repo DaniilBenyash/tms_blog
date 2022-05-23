dd679a2c5cf02bca254bd5f6924593575042849d
DaniilBenyash <daniilbenyash22@gmail.com>
2022-05-23 19:50:06 +0300

    refactor: add redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,6 @@ function App() {
             </div>
           )
           })}
-        <Button text='Primary' onClick={toggleTheme} className='button--primary' disabled={false}/>
-        <Button text='Secondary' onClick={() => console.log('Text')} className='button--secondary' disabled={false}/>
-        <Button text='Secondary 2' onClick={() => console.log('Text')} className='button--secondary2' disabled={false}/>
-        <Button text='Button with icon' onClick={() => console.log('Text')} className='button--with_icon' disabled={false} icon={<Bookmark />}/>
-        <Button onClick={() => console.log('Text')} className='button--like' disabled={false} icon={<Like />}/>
-        <Button onClick={() => console.log('Text')} className='button--dislike' disabled={false} icon={<Dislike />}/>
     </div>
   );
 }
