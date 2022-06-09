36307decdfbb87cb50a0742d2e305f28d00e6eb2
DaniilBenyash <daniilbenyash22@gmail.com>
2022-06-09 20:07:18 +0300

    fix: fetch searchPost

--- src/sagas/searchPostSaga.ts
+++ src/sagas/searchPostSaga.ts
@@ -7,7 +7,7 @@ import { type typePostsServer } from '../features/searchPosts/searchPostsSlice';
 export function* searchPosts(action: PayloadAction<string>){
     try {
         
-        const response: Response = yield fetch(`https://studapi.teachmeskills.by/blog/posts/?search=${action.payload}`)
+        const response: Response = yield fetch(`https://studapi.teachmeskills.by/blog/posts/?limit=1000&offset=0&search=${action.payload}`)
         const data: typePostsServer = yield(response.json())
         yield put(searchPostsSuccess(data))
         
